b182a618208db12613212fdbca3d597c4e2d1d92
Honey_space <ritmac@mail.ru>
2019-12-14 13:51:38 +0700

    Hfix

--- Flying Star/Assets/Classes/Passager.cs
+++ Flying Star/Assets/Classes/Passager.cs
@@ -11,21 +11,36 @@ public class Passager : MonoBehaviour
     public static Sprite[] HeadSet;
     public static Sprite[] DreamSet;    
     public static Sprite[] TipsSet;
+    public static Sprite[] PantsSet;
     public static Sprite[] TerrorTipsSet;    
-    Sprite Hair, Head,Body,Draem;
+    Sprite Hair, Head,Body,Draem, Pants;
     Sprite[] Dreams;
     Sprite[] Tips;  
-    GameObject HeadObj, BodyObj, DreamObj;    
+    GameObject DreamObj;    
     void Start()
     {
-        
+        SetStyle();
     }
     
     void SetStyle()
     {
+        int CountTip = Random.Range(1, 3);
+        int CountDream = Random.Range(2, 5);
         Hair = HairSet[Random.Range(0, HairSet.Length)];
         Body = BodySet[Random.Range(0, BodySet.Length)];
         Head = HeadSet[Random.Range(0, HeadSet.Length)];
+        Pants = PantsSet[Random.Range(0, PantsSet.Length)];
+        for (int i = 0;i< Random.Range(4, 7);i++)
+        {
+            GameObject tip = new GameObject();             
+            tip.AddComponent<SpriteRenderer>();
+            SpriteRenderer sr = tip.GetComponent<SpriteRenderer>();
+            if (i < CountTip)
+                sr.sprite = TerrorTipsSet[Random.Range(0, TerrorTipsSet.Length)];
+            Instantiate(sr, gameObject.transform);
+        }
+        for(int i =0;i<CountDream;i++)
+            Dreams[i] = DreamSet[Random.Range(0, DreamSet.Length)];
     }
 
     // Update is called once per frame
