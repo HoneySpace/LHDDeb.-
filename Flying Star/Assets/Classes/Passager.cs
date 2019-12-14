00f4debde2c9eeb557d175eb9de8d5cac3a98620
Honey_space <ritmac@mail.ru>
2019-12-14 12:33:39 +0700

    Up to data

---
+++ Flying Star/Assets/Classes/Passager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Passager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject Seat;
+    static Sprite[] HairSet;
+    static Sprite[] BodySet;
+    static Sprite[] DreamSet;
+    Sprite Hair,Body,Draem;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
