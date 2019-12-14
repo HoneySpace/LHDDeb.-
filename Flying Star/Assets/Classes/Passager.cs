28b27fdfa765ea67a216c12b9953535c15536ae7
Honey_space <ritmac@mail.ru>
2019-12-14 13:49:56 +0700

    Ready to generate passagers

--- Flying Star/Assets/Classes/Passager.cs
+++ Flying Star/Assets/Classes/Passager.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class Passager : MonoBehaviour
 {
-    // Start is called before the first frame update        
-    static Sprite[] HairSet;
-    static Sprite[] BodySet;
-    static Sprite[] DreamSet;
-    static Sprite[] TipsSet;
-    Sprite Hair,Body,Draem;
-    Sprite[] Tips;
+    // Start is called before the first frame update 
+    [SerializeField]
+    public static Sprite[] HairSet;
+    public static Sprite[] BodySet;
+    public static Sprite[] HeadSet;
+    public static Sprite[] DreamSet;    
+    public static Sprite[] TipsSet;
+    public static Sprite[] TerrorTipsSet;    
+    Sprite Hair, Head,Body,Draem;
+    Sprite[] Dreams;
+    Sprite[] Tips;  
     GameObject HeadObj, BodyObj, DreamObj;    
     void Start()
     {
         
+    }
+    
+    void SetStyle()
+    {
+        Hair = HairSet[Random.Range(0, HairSet.Length)];
+        Body = BodySet[Random.Range(0, BodySet.Length)];
+        Head = HeadSet[Random.Range(0, HeadSet.Length)];
     }
 
     // Update is called once per frame
---
+++ Flying Star/Assets/Classes/SpriteManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteManager : MonoBehaviour
+{
+    public Sprite[] HairSet;
+    public Sprite[] BodySet;
+    public Sprite[] HeadSet;
+    public Sprite[] DreamSet;
+    public Sprite[] TipsSet;
+    public Sprite[] PantsSet;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Passager.BodySet = BodySet;
+        Passager.HeadSet = HeadSet;
+        Passager.HairSet = HeadSet;
+        Passager.DreamSet = DreamSet;        
+        Passager.TipsSet = TipsSet;
+        Passager.PantsSet = PantsSet;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
